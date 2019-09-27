565de0271edf0b26cf8ea398622acfd11606f6a3
Jorge Castellon <jorge@thoriumdesign.com>
2019-09-26 19:26:46 -0700

    Created Admin page

---
+++ pages/admin.vue
@@ -0,0 +1,11 @@
+<template>
+    
+</template>
+
+<script id="admin-config-script" type="text/javascript">
+    var env = {
+        siteId: "bcxgnwgy3wkp4w",
+        local: false
+    };
+</script>
+<script type="application/javascript" src="https://local.forestry.io/forestry.min.js"></script>
