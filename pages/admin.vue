3c0aadd39dec63abe1a95c632593ee3be27f57f4
Jorge Castellon <jorge@thoriumdesign.com>
2019-09-26 19:32:22 -0700

    added admin page

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,11 +1,3 @@
 <template>
-    
+    <h1>Login</h1>
 </template>
-
-<script id="admin-config-script" type="text/javascript">
-    var env = {
-        siteId: "bcxgnwgy3wkp4w",
-        local: false
-    };
-</script>
-<script type="application/javascript" src="https://local.forestry.io/forestry.min.js"></script>
