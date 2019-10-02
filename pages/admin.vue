8df5d36806565f04af66ff696403875ce8d63ace
Jorge Castellon <jorge@thoriumdesign.com>
2019-10-02 11:11:28 -0700

    Readded admin page for generate

---
+++ pages/admin.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div>
+        <div id="app"></div>
+        <script id="admin-config-script" type="text/javascript">
+            var env = {
+                siteId: "tznupki6g-l7ca",
+                local: false
+            };
+        </script>
+    </div>
+</template>
+
+<script>
+export default {
+    head () {
+        return {
+            link: [
+                { rel: 'stylesheet', href: 'https://local.forestry.io/main.css' },
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans' },
+            ],
+            script: [
+                { src: 'https://local.forestry.io/forestry.min.js', async: true, defer: true, body: true }
+            ]
+        }
+    }
+}
+</script>
